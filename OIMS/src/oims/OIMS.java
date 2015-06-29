4276742bc6019d4f1505d5e4210825cf7039c995
unknown <ezouyyi@CN00084450.ericsson.se>
2015-06-29 10:30:37 +0800

    [oims] modify gitignore files

--- OIMS/src/oims/OIMS.java
+++ OIMS/src/oims/OIMS.java
@@ -4,16 +4,6 @@
  * and open the template in the editor.
  */
 package oims;
-import com.google.common.collect.Maps;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import  oims.config.ConfigManager;
-import  oims.dataBase.*;
-import  oims.dataBase.MYSQL.*;
 /**
  *
  * @author ezouyyi
